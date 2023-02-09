45fbb3183e38f520c5e013bdf54a5f151e2eff8c
Siddhartha Mishra <72318545+siddharthamishra1207@users.noreply.github.com>
2023-02-09 23:54:52 +0530

    Update Bitmanipulation.java
    & and |

--- Bitmanipulation.java
+++ Bitmanipulation.java
@@ -1,6 +1,9 @@
 public class Bitmanipulation{
 
     public static void main(String[] args) {
+        //or
        System.out.println(5|6);  
+        //and
+        System.out.println(5&6);  
     }   
 }
